d43ecfda5b43693f29e237ef080e34b267012596
ShanaDingcog <shandinangwatan@gmail.com>
2026-04-15 09:23:24 +0800

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'sb_publishable_Ydj6o1KncNQmVA2Ps6Zy6g_j2iGDXs'
-const supabaseKey = 'sb_secret_IJeXE9gBa3lIjLDJQI-avQ_C-EX8_Ch'
+const supabaseKey = 'sb_secret_IJeXE9gBa3lIjLDJQI-avQ_C-EX8_C'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
